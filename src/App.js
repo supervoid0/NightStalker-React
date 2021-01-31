1614f90acfc8070039519b233478ee039926185c
supervoid0 <shabee.neil@gmail.com>
2021-02-01 03:44:22 +0600

    height fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MainContent from "./componants/MainContent"
 
 function App() {
   return (
-    <div className="App bg-secondary h-screen">
+    <div className="App bg-secondary h-full">
       <Layout>
         <MainContent/>
         
